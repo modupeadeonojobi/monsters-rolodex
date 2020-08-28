f785c8e990d4d325ecac94fc297e053d80f47b18
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2020-08-28 04:36:00 +0100

    Updates

---
+++ precache-manifest.8d969b655f912d7c2b1be22070328cb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba18959fcc388003444c53a89440040a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c8178344025c632c788",
+    "url": "/monsters-rolodex/static/css/main.3f9e1230.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8178344025c632c788",
+    "url": "/monsters-rolodex/static/js/main.b9c20438.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
